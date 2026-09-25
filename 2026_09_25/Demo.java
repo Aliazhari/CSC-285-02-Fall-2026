a705ccafc5368586b81c640c7e47233c401303c2
Ali Azhari <aliazhari@gmail.com>
2026-09-25 11:52:53 -0400

    JUnit

---
+++ 2026_09_25/Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        
+        String n1 = "Hello";
+        String n2 = "Hello";
+        if ( n1 == n2) {
+            System.out.println("True");
+            
+        }
+        else System.out.println("Not the same");
+    }
+    
+}
